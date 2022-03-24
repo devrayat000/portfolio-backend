b8c50f3efc3a1af9f0cc09357b9fa1f8128a23c2
devrayat000 <2010050@me.buet.ac.bd>
2022-03-24 13:07:18 +0600

    config solve

--- config.ts
+++ config.ts
@@ -1,18 +1,18 @@
 // 3000 is standard for node apps
 // Once deployed, Heroku will supply this var to your app
-export const PORT = parseInt(process.env.PORT) ?? 8000;
+export const PORT = parseInt(process.env.PORT) || 8000;
 
 // Postgres DB URL
 // The default value here will work if you've installed Postgres on MacOS using brew
 // One the app is deployed to Heroku, this var will be supplied by the Postgres addon
 export const DATABASE_URL =
-  process.env.APPSETTING_DATABASE_URL ??
-  process.env.DATABASE_URL ??
+  process.env.APPSETTING_DATABASE_URL ||
+  process.env.DATABASE_URL ||
   `postgres://postgres:ppooii12@localhost/portfolio`;
 
 // Default to 30 days
 export const SESSION_MAX_AGE =
-  parseInt(process.env.SESSION_MAX_AGE) ?? 60 * 60 * 24 * 30;
+  parseInt(process.env.SESSION_MAX_AGE) || 60 * 60 * 24 * 30;
 
 // If the environment doesn't supply a value, default the secret to a secure random string
 // This will cause all cookies to be invalidated with each app restart (annoying but better than having a hardcoded default)
