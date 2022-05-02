cb1cd52bbafb08e7582e95b1efa0dff6e7b3f686
devrayat000 <2010050@me.buet.ac.bd>
2022-05-03 01:27:44 +0600

    bug fix (project)

--- models/project.ts
+++ models/project.ts
@@ -1,13 +1,13 @@
-import { list } from "@keystone-6/core";
-import { relationship, select, text, timestamp } from "@keystone-6/core/fields";
-import { document } from "@keystone-6/fields-document";
+import { list } from '@keystone-6/core'
+import { relationship, select, text, timestamp } from '@keystone-6/core/fields'
+import { document } from '@keystone-6/fields-document'
 
-import { Lists } from ".keystone/types";
-import { Prisma } from ".prisma/client";
+import { Lists } from '.keystone/types'
+import { Prisma } from '.prisma/client'
 
-import { hasApiKey, isAdmin } from "../utils/access";
+import { hasApiKey, isAdmin } from '../utils/access'
 
-export const Project: Lists["Project"] = list({
+export const Project: Lists['Project'] = list({
   access: {
     operation: {
       query: hasApiKey,
@@ -36,63 +36,63 @@ export const Project: Lists["Project"] = list({
     }),
     source: text(),
     status: select({
-      type: "enum",
-      defaultValue: "draft",
-      ui: { displayMode: "segmented-control" },
+      type: 'enum',
+      defaultValue: 'draft',
+      ui: { displayMode: 'segmented-control' },
       options: [
-        { label: "Published", value: "published" },
-        { label: "Draft", value: "draft" },
+        { label: 'Published', value: 'published' },
+        { label: 'Draft', value: 'draft' },
       ],
       hooks: {
         async beforeOperation({ operation, item, context, resolvedData }) {
           if (
-            operation === "update" &&
-            (resolvedData as Prisma.ProjectUpdateInput).status === "published"
+            operation === 'update' &&
+            (resolvedData as Prisma.ProjectUpdateInput).status === 'published'
           ) {
             context.query.Project.updateOne({
               data: { publishedAt: new Date() },
               where: { id: item.id },
-            });
+            })
           }
         },
         async afterOperation({ operation, item, context, resolvedData }) {
           if (
-            operation === "create" &&
-            (resolvedData as Prisma.ProjectUpdateInput).status === "published"
+            operation === 'create' &&
+            (resolvedData as Prisma.ProjectUpdateInput).status === 'published'
           ) {
             context.query.Project.updateOne({
               data: { publishedAt: new Date() },
               where: { id: item.id },
-            });
+            })
           }
         },
       },
     }),
     tags: relationship({
-      ref: "Tag.projects",
+      ref: 'Tag.projects',
       ui: {
-        displayMode: "cards",
-        cardFields: ["name"],
-        inlineEdit: { fields: ["name"] },
+        displayMode: 'cards',
+        cardFields: ['name'],
+        inlineEdit: { fields: ['name'] },
         linkToItem: true,
         inlineConnect: true,
-        inlineCreate: { fields: ["name"] },
+        inlineCreate: { fields: ['name'] },
       },
       many: true,
     }),
     images: relationship({
-      ref: "Image",
+      ref: 'Image',
       many: true,
       ui: {
-        displayMode: "cards",
-        cardFields: ["image", "label"],
-        inlineEdit: { fields: ["image", "label"] },
+        displayMode: 'cards',
+        cardFields: ['image', 'label'],
+        inlineEdit: { fields: ['image', 'label'] },
         linkToItem: true,
         inlineConnect: true,
-        inlineCreate: { fields: ["image", "label"] },
+        inlineCreate: { fields: ['image', 'label'] },
       },
     }),
     publishedAt: timestamp(),
-    createdAt: timestamp({ defaultValue: { kind: "now" } }),
+    createdAt: timestamp({ defaultValue: { kind: 'now' } }),
   },
-});
+})
