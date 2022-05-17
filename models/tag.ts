29fe348c38a4a316c37408d6ccce01437a7f8431
devrayat000 <2010050@me.buet.ac.bd>
2022-05-17 19:52:25 +0600

    added virtual tag slug

--- models/tag.ts
+++ models/tag.ts
@@ -1,5 +1,7 @@
-import { list } from '@keystone-6/core'
-import { relationship, text } from '@keystone-6/core/fields'
+import { graphql, list } from '@keystone-6/core'
+import { relationship, text, virtual } from '@keystone-6/core/fields'
+import { snakeCase } from 'lodash'
+
 import { hasApiKey, isAdmin } from '../utils/access'
 import { Lists } from '.keystone/types'
 
@@ -19,6 +21,14 @@ export const Tag: Lists['Tag'] = list({
     name: text({
       validation: { isRequired: true },
       db: { isNullable: false },
+    }),
+    slug: virtual({
+      field: graphql.field({
+        type: graphql.String,
+        resolve(parent) {
+          return snakeCase(parent.name)
+        },
+      }),
     }),
     projects: relationship({ ref: 'Project.tags', many: true }),
   },
